6463d7b56f18bb97cf37c32dbc4e56090db41f77
abdelali el makkioui <abdelmkey19973@gmail.com>
2024-07-27 00:14:15 +0100

    add handle multiple connections

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -62,7 +62,7 @@ fun main() {
         // Uncomment this block to pass the first stage
         var serverSocket = ServerSocket(4221)
         //
-        // // Since the tester restarts your program quite often, setting SO_REUSEADDR
+        //// Since the tester restarts your program quite often, setting SO_REUSEADDR
         // // ensures that we don't run into 'Address already in use' errors
         serverSocket.reuseAddress = true
         //
